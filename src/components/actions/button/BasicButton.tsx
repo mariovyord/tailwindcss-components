f95aba0637f8bcc8576d7e71e82673caa8f33958
NecroBread <mariovyordanov@gmail.com>
2022-09-05 23:25:33 +0300

    update: fix minor warning

--- src/components/actions/button/BasicButton.tsx
+++ src/components/actions/button/BasicButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BasicButton = () => {
 	return (
-		<button className='flex h-12 w-full items-center justify-center rounded-lg bg-lime-700 px-6 font-semibold text-white  hover:bg-lime-800 hover:shadow sm:w-auto'>Button</button>
+		<button type='button' className='flex h-12 w-full items-center justify-center rounded-lg bg-lime-700 px-6 font-semibold text-white  hover:bg-lime-800 hover:shadow sm:w-auto'>Button</button>
 	)
 }
 
