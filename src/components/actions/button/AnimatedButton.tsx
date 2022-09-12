2dbc5f2124ea87290d0f269e76b38d50dcb35fdd
NecroBread <mariovyordanov@gmail.com>
2022-09-12 21:27:10 +0300

    add: disabled styles

--- src/components/actions/button/AnimatedButton.tsx
+++ src/components/actions/button/AnimatedButton.tsx
@@ -2,7 +2,7 @@ const AnimatedButton = () => {
     return (
         <button
             type='button'
-            className='relative flex h-12 w-full items-center justify-center overflow-hidden rounded-lg bg-primary px-6 font-semibold text-white transition-all after:absolute after:top-0 after:h-full after:w-full after:-translate-x-full after:rounded-lg after:bg-primary-focus after:transition-all hover:shadow hover:after:translate-x-0 active:scale-95 sm:w-auto'
+            className='relative flex h-12 w-full items-center justify-center overflow-hidden rounded-lg bg-primary px-6 font-semibold text-white transition-all after:absolute after:top-0 after:h-full after:w-full after:-translate-x-full after:rounded-lg after:bg-primary-focus after:transition-all hover:shadow hover:after:translate-x-0 active:scale-95 disabled:pointer-events-none disabled:opacity-60 sm:w-auto'
         ><span className="z-10">Button</span></button>
     )
 }
--- src/components/actions/button/BasicButton.tsx
+++ src/components/actions/button/BasicButton.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const BasicButton = () => {
-	return (
-		<button type='button' className='flex h-12 w-full items-center justify-center rounded-lg bg-lime-700 px-6 font-semibold text-white transition-all hover:bg-lime-800 hover:shadow active:scale-95 sm:w-auto'>Button</button>
-	)
+    return (
+        <button
+            type='button'
+            className='flex h-12 w-full items-center justify-center rounded-lg bg-lime-700 px-6 font-semibold text-white transition-all hover:bg-lime-800 hover:shadow active:scale-95 disabled:pointer-events-none disabled:opacity-60 sm:w-auto'
+        >Button</button>
+    )
 }
 
 export default BasicButton;
